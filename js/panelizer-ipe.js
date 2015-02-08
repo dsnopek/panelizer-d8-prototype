2bda3f9ddba5b96cef8ac85350abe952018f0a51
hefox <hefox@426416.no-reply.drupal.org>
2015-02-08 13:48:03 -0500

    Issue #1982654 by hefox, DamienMcKenna, mglaman: Repeatially asked to confirm when trying to save a default for a node type.

--- js/panelizer-ipe.js
+++ js/panelizer-ipe.js
@@ -16,18 +16,22 @@ Drupal.behaviors.PanelizerIPE = {
     }
 
     // Update the default with this display.
-    $('input#panelizer-save-default', context).bind('mouseup', function (e) {
-      if (!confirm(Drupal.t("This will save this configuration as the new default for all entities of this type. This action cannot be undone. Are you sure?"))) {
-        this.ipeCancelThis = true;
-        return false;
-      }
+    $('input#panelizer-save-default', context).once('save-default-alert', function() {
+      $(this).bind('mouseup', function (e) {
+        if (!confirm(Drupal.t("This will save this configuration as the new default for all entities of this type. This action cannot be undone. Are you sure?"))) {
+          this.ipeCancelThis = true;
+          return false;
+        }
+      });
     });
 
     // Redirect to confirmation page.
-    $('input#panelizer-ipe-revert', context).bind('mouseup', function (e) {
-      window.location.href = Drupal.settings.panelizer.revert_default_url;
-      this.ipeCancelThis = true;
-      return false;
+    $('input#panelizer-ipe-revert', context).once('revert-alert', function() {
+      $(this).bind('mouseup', function (e) {
+        window.location.href = Drupal.settings.panelizer.revert_default_url;
+        this.ipeCancelThis = true;
+        return false;
+      });
     });
   }
 };
