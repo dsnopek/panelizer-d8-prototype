b81ab14b0aff537bea74ff41395b5078506a9e8a
Damien McKenna <damien@mc-kenna.com>
2015-02-08 11:25:56 -0500

    Issue #2311453 by DamienMcKenna, drupalninja: Redirect IPE to normal 'revert' page to revert the display.

--- js/panelizer-ipe.js
+++ js/panelizer-ipe.js
@@ -9,26 +9,27 @@
 
 Drupal.behaviors.PanelizerIPE = {
   attach: function (context) {
+    // Disable the Leave Page dialog warning.
     if ($(context).is('form#panels-ipe-edit-control-form.panels-ipe-edit-control-form')) {
-      // Disable the Leave Page dialog warning
       window.onbeforeunload = null;
       window.onunload = null;
     }
+
+    // Update the default with this display.
     $('input#panelizer-save-default', context).bind('mouseup', function (e) {
       if (!confirm(Drupal.t("This will save this configuration as the new default for all entities of this type. This action cannot be undone. Are you sure?"))) {
         this.ipeCancelThis = true;
         return false;
       }
     });
+
+    // Redirect to confirmation page.
     $('input#panelizer-ipe-revert', context).bind('mouseup', function (e) {
-      if (!confirm(Drupal.t("This will remove all panelizer configuration for this entity and revert it to default settings. This action cannot be undone. Are you sure?"))) {
-        this.ipeCancelThis = true;
-        return false;
-      }
+      window.location.href = Drupal.settings.panelizer.revert_default_url;
+      this.ipeCancelThis = true;
+      return false;
     });
   }
 };
 
 })(jQuery);
-
-
